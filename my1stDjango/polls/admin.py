19339693ec56ca6d9b8ba461e61a6b53ba5f9624
Yves LAMBERT <yves.lambert@fms-ea.com>
2024-10-14 15:30:07 +0200

    quelques modifs, ça tourne

--- my1stDjango/polls/admin.py
+++ my1stDjango/polls/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+    list_filter = ['question_text', 'pub_date']
+    ordering = ['pub_date', 'question_text']
+    search_fields = ['question_text', 'pub_date']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['question', 'choice_text', 'votes']
+    list_filter = ['question', 'choice_text', 'votes']
+    ordering = ['question', 'choice_text', 'votes']
+    search_fields = ['choice_text']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
